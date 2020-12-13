1617a4a3e65e26e9568e1f306cba57929bac85f4
Lilmarziq <ammar-haziq.bin-mohd-halim@stud.hshl.de>
2020-12-13 23:04:23 +0100

    Create smartLamp.h

---
+++ Project_08/smartLamp.h
@@ -0,0 +1,16 @@
+#ifndef SMART_LAMP
+#define SMART_LAMP
+
+class smartLamp
+{
+    public:
+    
+    bool thiefDetected;
+    bool lampOn;
+    
+    void blink();
+    
+    void smartLampOn();
+}
+
+#endif
