27f9a0e4a4042baf4dcca16e3cc4f769a6dfa5f6
Lilmarziq <ammar-haziq.bin-mohd-halim@stud.hshl.de>
2021-01-04 10:51:45 +0100

    Update smartAlarm.cpp

--- SmartHome/smartAlarm/Source code and header files/smartAlarm.cpp
+++ SmartHome/smartAlarm/Source code and header files/smartAlarm.cpp
@@ -1,8 +1,64 @@
 #include "smartAlarm.h"
 #include <iostream>
 
-void smartAlarm :: ring(){
-    
+
+void ring()
+{
+    volume = emergencyVolume;
+    on();
+}
+
+void on()
+{
+    cout << "ON";
+}
+
+void getInputHub()
+{
+    int input;
+    cin >> input;
+    if (input == 0)
+    {
+        
+    }
+    int input;
+    cin >> input;
+    else if (input == 1)
+    {
+        ring();
+    }
+    int input;
+    cin >> input;
+    else if (input == 2)
+    {
+        setAlarm(int mode, double Volume, double time);
+        cin >> mode;
+        cin >> volume;
+        cin >> time;
+    }
+    else if (input == 3)
+    {
+        theifDetected = true;
+    }
+    else if (input == 4)
+    {
+        state = 0;
+    }
+    else if (input == 5)
+    {
+        state = 1;
+    else if (input == 6)
+    {
+        state = 2;
+
+    }
+    }
+}
+
+void setAlarm(int mode, double Volume, double time)
+{
+    MODE = mode;
+    alarmVolume = Volume;
 }
 
 smartAlarm :: smartAlarm(controlHub* newController){
