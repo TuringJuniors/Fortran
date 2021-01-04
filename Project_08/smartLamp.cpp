f38a4859b1086ab8d52c29b5283c94da3eaa3c19
hadimran323 <hadi-imran.bin-md-radzi@stud.hshl.de>
2021-01-04 11:02:52 +0100

    Update smartLamp.cpp

--- Project_08/smartLamp.cpp
+++ Project_08/smartLamp.cpp
@@ -2,12 +2,19 @@
 #include <iostream>
 
 void smartLamp :: blink(){
-    
+    while(1){
+    	on();
+    	off();
+	}
 }
 
 void smartLamp :: on(){
-    
+    cout<<lampOn;
 }
+
+void smartLamp :: off(){
+	cout<<lampOff;
+} 
 
 smartLamp :: smartLamp(controlHub* newController){
     
