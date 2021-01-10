df65fe7037a601bcd55f448767f4e36799d33d9c
hadimran323 <hadi-imran.bin-md-radzi@stud.hshl.de>
2021-01-10 19:07:39 +0100

    Update smartLamp.h

--- SmartHome/smartLamp/Source code and header/smartLamp.h
+++ SmartHome/smartLamp/Source code and header/smartLamp.h
@@ -5,11 +5,18 @@ using namespace std;
 #ifndef SMART_LAMP
 #define SMART_LAMP
 
-class controlHub;
+class controlHub{
+	
+};
 class smartLamp
 {
     public:
     
+    smartLamp(controlHub* newController);
+    
+    void getInputControlHub();
+    
+    int state;
     
     void blink();
     
@@ -17,11 +24,7 @@ class smartLamp
     
     void off();
     
-    smartLamp(controlHub* newController);
-    
-    void getInputControlHub();
-    
-    int state;
+   
     
     private:
     
