1ed2e7817c16802d816c0703408952195d186aa4
Adib6637 <72360300+Adib6637@users.noreply.github.com>
2021-02-26 22:56:15 +0100

    Update Smartdoor.cpp

--- SmartHome/Smart door/Source code and header/Smartdoor.cpp
+++ SmartHome/Smart door/Source code and header/Smartdoor.cpp
@@ -1,11 +1,4 @@
-/******************************************************************************
 
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <iostream>
 #include "smartdoor.h"
 using namespace std;
@@ -18,11 +11,6 @@ int main()
 {
 	controlHub app;
 	smartdoor door1(&app);
-
-
-
-
-
 
 	switch (door1.state) {
 	case idleState:
@@ -55,6 +43,4 @@ int main()
 		}
 		break;
 	}
-	
-
 }
